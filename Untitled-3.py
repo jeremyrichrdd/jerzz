d40443ba80158696e2227faaed7d5669c8c431e6
jeremyrichrdd <jeremyxrichrd@gmail.com>
2026-08-02 23:46:23 +0700

    Delete Untitled-3.py

--- Untitled-3.py
+++
@@ -1,8 +0,0 @@
-input = int(input("masukkan nilai mu = "))
-
-i=1
-total=0
-while i <= input:
-    total = total + i  
-    i = i+1    
-print(f"jadi, nilai total dari 1 sampe {input} adalah {total}")
